f941a3847cac27bdf3f77d3b21c69909681129de
Chandradeo Arya <chandradeoaryaarya@gmail.com>
2023-06-12 21:01:01 +0530

    adding code

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ application.add_url_rule('/', 'index', (lambda:html))
  
 if __name__ == "__main__":
     application.debug = True
-    application.run(host='0.0.0.0', port=8000)
+    application.run(host='0.0.0.0', port=5000)
